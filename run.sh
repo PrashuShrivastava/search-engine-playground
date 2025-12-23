80f6c217f796f84e7545b585e5382147805e9ab9
Pranshu Shrivastava <pranshupratap1008@gmail.com>
2025-12-23 20:40:14 +0000

    Update run.sh to log to ./search-engine-playground/app.log (within project dir)

--- run.sh
+++ run.sh
@@ -40,6 +40,6 @@ else
 fi
 
 echo "Running SearchEngineApp with classpath: $CP"
-java -Dorg.slf4j.simpleLogger.logFile=app.log \
+java -Dorg.slf4j.simpleLogger.logFile=./search-engine-playground/app.log \
      -Dorg.slf4j.simpleLogger.defaultLogLevel=info \
      -cp "$CP" com.searchplayground.core.SearchEngineApp
